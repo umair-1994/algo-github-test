20ebd6e243cf289a9d896847c41df7b4a1eeb3a4
umair-1994 <umaircheema1994@gmail.com>
2021-06-30 15:42:44 +0500

    rename the fucntions name in the test methods file

--- test_algo_methods.py
+++ test_algo_methods.py
@@ -1,4 +1,4 @@
-from python_test import open_file,parse_commands, copy_commands, random_commands, functional_commands
+from python_test import open_file, parse_command, copy_command, random_command, functional_command
 import pytest
 from unittest import mock
 
@@ -14,47 +14,47 @@ def test_exception_file_not_found():
 
 
 def test_parse_commads():
-    assert parse_commands()
+    assert parse_command()
 
 
 def test_exception_on_parse_command():
     with pytest.raises(ValueError):
-        parse_commands()
+        parse_command()
         print("Data not found")
 
 
 def test_copy_commads():
-    assert copy_commands()
+    assert copy_command()
 
 
 def test_exception_on_copy_commands():
     with pytest.raises(ValueError):
-        copy_commands()
+        copy_command()
         print("Data not found")
 
 
 def test_functional_commads():
-    assert functional_commands()
+    assert functional_command()
 
 
 def test_exception_on_functional_commands():
     with pytest.raises(ValueError):
-        functional_commands()
+        functional_command()
         print("Data not found")
 
 
 def test_random_commads():
-    assert random_commands()
+    assert random_command()
 
 
 def test_exception_on_random_commands():
     with pytest.raises(ValueError):
-        random_commands()
+        random_command()
         print("Data not found")
 
 
 def test_mock_random_commands():
-    fixed_value = mock.Mock(name="mock obect", return_value=2)
-    if random_commands() == fixed_value():
+    fixed_value = mock.Mock(name="mock object", return_value=2)
+    if random_command() == fixed_value():
         pass
 
